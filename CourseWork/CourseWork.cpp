69076ae421991aabbecc5c062cd4a7d029d5d705
Самсон Квачадзе <lat.lesnik@gmail.com>
2020-12-21 14:17:32 +0300

    finished coursework

--- CourseWork/CourseWork.cpp
+++ CourseWork/CourseWork.cpp
@@ -1,20 +1,80 @@
-// CourseWork.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+int Laba1();
+int Laba2();
+int Laba3();
+int Laba4();
+short int coursemainmenu();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	short int choose_menu, exit = 0;
+
+	while (1)
+	{
+		choose_menu = coursemainmenu();
+		switch (choose_menu)
+		{
+		case 1:
+			system("cls");
+			cout << "Лабораторная работа №1 \n";
+			Laba1();
+			system("cls");
+			break;
+		case 2:
+			system("cls");
+			Laba2();
+			system("pause");
+			system("cls");
+			break;
+		case 3:
+			system("cls");
+			Laba3();
+			system("pause");
+			system("cls");
+			break;
+		case 4:
+			system("cls");
+			Laba4();
+			system("cls");
+			break;
+		case 0:
+			return -1;
+			break;
+
+		}
+	}
+
+	return 0;
+}
+	
+short int coursemainmenu() // меню, выбор задания
+{
+	setlocale(LC_ALL, "Russian");
+	short int i;
+	cout << "Курсовая работа \n\n";
+	cout << "Выберите один из пунктов меню (1-4), чтобы выполнить соответствующую ей лабораторную работу, или 0, чтобы выйти. \n";
+	cout << "1) Лабораторная работа №1 \n";
+	cout << "2) Лабораторная работа №2 \n";
+	cout << "3) Лабораторная работа №3 \n";
+	cout << "4) Лабораторная работа №4 \n";
+	cout << "0) Выход\n" << endl;
+
+	cout << "Выбор: ";
+
+	while (1)
+	{
+		cin >> i;
+		if ((i >= 0) & (i <= 4))
+			break;
+		else
+			cout << "Некорректный ввод, повторите попытку.\n";
+	}
+	cout << "\n";
+	return i;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ CourseWork/Laba1.cpp
@@ -0,0 +1,136 @@
+// Laba1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int Laba1()
+{
+	setlocale(0, "");
+	std::cout << "Output format: \" sizeof(<data_type_here>) <Size_of_given_data_type_here>.\"" << std::endl;
+
+	std::cout << "sizeof(int)         "<< sizeof(int)			<< std::endl;
+	std::cout << "sizeof(short int)   "<< sizeof(short int)		<< std::endl;
+	std::cout << "sizeof(long int)    "<< sizeof(long int)		<< std::endl;
+	std::cout << "sizeof(float)       "<< sizeof(float)			<< std::endl;
+	std::cout << "sizeof(double)      "<< sizeof(double)		<< std::endl;
+	std::cout << "sizeof(long double) "<< sizeof(long double)   << std::endl;
+	std::cout << "sizeof(char)        "<< sizeof(char)          << std::endl;
+	std::cout << "sizeof(bool)        "<< sizeof(bool)			<< std::endl;
+	std::cout << std::endl;
+
+	int inta;
+	unsigned int order = 32; // Amount of digits
+	unsigned int mask = 1 << order - 1;// Comparison mask
+	std::cout << "Input inta: ";
+	std::cin >> inta;
+	std::cout << std::endl;
+	std::cout << "Memory representation of integer-type variable: " << std::endl;
+	for (int i = 1; i <= order; i++)
+	{
+		putchar(inta & mask ? '1' : '0');
+		inta <<= 1;
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+	}
+	std::cout << std::endl << std::endl;
+	std::cout << "Memory representation of float-type variable: " << std::endl;
+	union {
+		int intb;
+		float floatb;
+	};
+	std::cout << "Input floatb: ";
+	std::cin >> floatb;
+	std::cout << std::endl;
+	for (int i = 1; i <= order; i++)
+	{
+		putchar(intb & mask ? '1' : '0');
+		intb <<= 1;
+		if ((i <= 9) and (i % 9 == 0))
+		{
+			putchar(' ');
+		}
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+
+	}
+	std::cout << std::endl << std::endl;
+	std::cout << "Memory representation of double-type variable: " << std::endl;
+	union {
+		int intarr [2];
+		double doubb = 3.14159265;
+	};
+	std::cout << "Input doubb: ";
+	std::cin >> doubb;
+	std::cout << std::endl;
+	for (int i = 1; i <= order; i++)
+	{
+		putchar(intarr[1] & mask ? '1' : '0');
+		intarr[1] <<= 1;
+		if ((i <= 16) and (i % 12 == 0))
+		{
+			putchar(' ');
+		}
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+	}
+	for (int i = 33; i <= (order*2); i++)
+	{
+		putchar(intarr[0] & mask ? '1' : '0');
+		intarr[0] <<= 1;
+	}
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Personal task: 11" << std::endl << std::endl;
+
+	int intc, N, F;
+	std::cout << "Enter intc value: ";
+	std::cin >> intc;
+	F = intc;
+	std::cout << "Before bit shifting: ";
+	for (int i = 1; i <= order; i++)
+	{
+		putchar(intc & mask ? '1' : '0');
+		intc <<= 1;
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+	}
+	std::cout << std::endl << std::endl;
+
+	intc = F;
+	std::cout << "Enter bit shifting N value: ";
+	std::cin >> N;
+	intc <<= N;
+	std::cout << "After bit shifting: ";
+	for (int i = 1; i <= order; i++)
+	{  
+		putchar(intc & mask ? '1' : '0');
+		intc <<= 1;
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+	}
+
+	std::cout << std::endl << std::endl;
+	system("pause"); 
+	return 0;
+} 
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
